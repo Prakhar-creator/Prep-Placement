4910a5014396d7a3d1d5f5305c0782b920f04c1e
Prakhar-creator <64277081+Prakhar-creator@users.noreply.github.com>
2022-02-26 09:52:25 +0530

    Create codes.java

---
+++ codes.java
@@ -0,0 +1,496 @@
+import java.util.Scanner;
+class Main
+{
+  	public static void main(String args[])
+  	{
+    	// code here
+      System.out.print("Hello World");
+  	}
+}
+//Hello World
+
+import java.util.Scanner;
+class Main{
+    public static void main(String args[])
+    {
+        //fill your code;
+      System.out.print("Hello World\tHello World");
+    }
+}
+//Hello World	Hello World
+
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        //fill your code;
+      System.out.println("Hello World");
+      System.out.print("Hello World");
+    }
+}
+
+//Hello World
+//Hello World
+
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        //fill the code;
+      Scanner sc=new Scanner(System.in);
+      char a=sc.nextLine().charAt(0);
+      System.out.print(a);
+    }
+}
+//z
+
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        //fill the code;
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      System.out.print(a);
+    }
+}
+//integer input
+
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        //fill your code;
+      Scanner sc=new Scanner(System.in);
+      float a=sc.nextFloat();
+      System.out.printf("%.2f",a);
+        
+    }
+}
+//56.2425
+//56.24
+
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        //fill your code;
+      Scanner sc=new Scanner(System.in);
+      String testString=sc.nextLine();
+      System.out.print(testString);
+    }
+}
+/*
+Programmer
+Programmer
+897
+897
+*/
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        // fill the code;
+      Scanner sc=new Scanner(System.in);
+      char uni=sc.nextLine().charAt(0);
+      int a=(int)uni;
+      System.out.print(a);
+      
+    }
+}
+/*
+g
+103
+*/
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        //fill the code;
+      Scanner sc=new Scanner(System.in);
+      int code=sc.nextInt();
+      char c=(char)code;
+      System.out.print(c);
+    }
+}
+// 103 , g 
+import java.util.Scanner;
+class Main
+{
+    	public static void main(String args[])
+        {
+            // fill your code
+          Scanner sc=new Scanner(System.in);
+          int a=sc.nextInt();
+          int b=sc.nextInt();
+          int sum=a+b;
+          int diff=a-b;
+          System.out.println(sum);
+          System.out.print(diff);
+        }
+}
+/*
+55
+
+34
+
+89
+
+21
+*/
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        // fill your code
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      int prod=a*b;
+      int quo=a/b;
+      int rem=a%b;
+      System.out.println(prod);
+      System.out.println(quo);
+      System.out.print(rem);
+      
+    }
+}
+/*
+50
+
+10
+
+500
+
+5
+
+0
+*/
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        // fill your code
+      Scanner sc=new Scanner(System.in);
+      int a =sc.nextInt();
+      int b=sc.nextInt();
+      int temp=a;
+      a=b;
+      b=temp;
+      System.out.println(a);
+      System.out.print(b);
+    }
+}
+/*
+20
+
+10
+10
+
+20
+*/
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        // fill your code
+      Scanner sc=new Scanner (System.in);
+      int a=sc.nextInt();
+      int b=sc.nextInt();
+      a=a+b;
+      b=a-b;
+      a=a-b;
+      System.out.println(a);
+      System.out.print(b);
+      
+    }
+}
+/*
+10
+
+20
+20
+
+10
+*/
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        // fill your code
+      Scanner sc=new Scanner(System.in);
+       float a=sc.nextInt();
+       float b=sc.nextInt();
+       float c=sc.nextInt();
+       float d=sc.nextInt();
+       float e=sc.nextInt();
+      double avg=(a+b+c+d+e)/5.00;
+      System.out.printf("%.2f",avg);
+      
+    }
+}
+/*
+10 
+
+20
+
+30
+
+40
+
+50
+30.00 */
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner obj=new Scanner(System.in);
+        int a=obj.nextInt();
+        int b=obj.nextInt();
+        int c=obj.nextInt();
+        float d=obj.nextFloat();
+        
+        int x=a*a;
+        int y=b*c;
+        double z=3.14*(d*d);
+        
+        System.out.printf(x+"\n"+y+"\n" +"%.2f",z);
+        
+    }
+}
+/*
+5
+
+5
+
+4
+
+2.0
+
+25
+
+20
+
+12.56
+*/
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        // fill your code
+      Scanner sc=new Scanner (System.in);
+      int pi=sc.nextInt(); 
+      int yr=sc.nextInt();
+      float rate=sc.nextFloat();
+      float si=(pi*rate*yr)/100;
+      System.out.printf("%.2f",si);
+    }
+}
+/*
+15000
+
+2
+
+2.8
+
+
+
+Sample Output:
+
+840.00
+*/
+import java.util.*;
+import java.lang.Math;
+
+class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.print(Math.pow(a,b));
+    }
+}
+/*
+2
+
+3 
+8
+*/
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        // fill your code
+      Scanner sc=new Scanner(System.in);
+      int num=sc.nextInt();
+      System.out.println((num%2==0) ? "Even" : "Odd");
+    }
+}
+/*
+5
+Odd
+*/
+import java.util.Scanner;
+class Main
+{
+  public static void main(String args[])
+  {
+    // fill your code
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int length=2*(a+b);
+    int quantity=a*b;
+    System.out.println(length+"m");
+    System.out.println(quantity+"sqm");
+    
+  }
+}
+  Back to lesson
+Section 1
+1. Fencing The Ground
+QUESTION 1 OF 1
+Fencing The Ground
+The college ground is rectangular in shape. Management decides to build a fence around the ground. In order to help the construction workers to build a straight fence, they planned to place a thick rope around the ground. They wanted to buy only the exact length of the rope that is needed. They also wanted to cover the entire ground with a thick carpet during the rainy season. They wanted to buy only the exact quantity of carpet that is needed. They requested your help. Can you please help them by writing a program to find the exact length of the rope and the exact quantity of carper that is required?
+Input Format:
+
+Input consists of 2 integers.
+
+The first integer corresponds to the length of the ground
+
+The second integer corresponds to the breadth of the ground.
+
+
+
+Output Format:
+
+Output consists of 2 integers.
+
+The first integer corresponds to the exact length of the rope
+
+The second integer corresponds to the exact quantity of carper.
+
+
+
+Sample Input:
+
+/*
+50
+
+20
+140m
+
+1000sqm
+*/
+import java.util.Scanner;
+class Main
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("The number of students in each team is "+a/b+" and left out is "+a%b);
+// fill your code
+}
+}
+/*
+60
+
+8
+
+The number of students in each team is 7 and left out is 4
+*/
+import java.util.Scanner;
+import java.lang.Math;
+class Main
+{
+public static void main(String args[])
+{
+Scanner obj=new Scanner(System.in);
+float a=obj.nextInt();
+float b=obj.nextInt();
+float c=obj.nextInt();
+float d=obj.nextInt();
+
+float v1=(a+c)/2;
+float v2=(b+d)/2;
+System.out.println("Binoy's house is located at ("+v1+","+v2+")");
+    
+}
+}
+/*
+/*
+2
+
+4
+
+10
+
+15
+Binoy's house is located at (6.0,9.5)
+*/
+import java.util.*;
+class Main
+{
+	public static void main (String args [])
+	{
+		Scanner in=new Scanner (System.in);
+		int sp,cp,p,l=0;
+		sp=in.nextInt();
+        cp=in.nextInt();
+        p=in.nextInt();
+        l=((sp*cp)-(sp*p))-100;
+  		System.out.println("The profit obtained is Rs."+l);
+
+	}
+}
+/*
+1000
+
+2
+
+1
+The profit obtained is Rs.900
+*/
+import java.util.Scanner;
+class Main
+{
+	public static void main(String args[])
+	{
+		// fill your code
+      Scanner sc=new Scanner (System.in);
+      int digit=sc.nextInt(); int rem=0; int sumofdigit=0;
+     while(digit>0){   rem=digit%10;
+       sumofdigit=sumofdigit+rem;
+      digit=digit/10; }
+      System.out.print("Alice must go in path-"+sumofdigit);
+      
+      
+	}
+}
+/*
+23
+Alice must go in path-5
+*/
+
+
+
+
+
+
+
